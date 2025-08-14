ac0c2e1de6fa098ddbdf5c6d91948aa17b40a4c0
DragoNHatake333 <dragondatosvideo@gmail.com>
2025-08-14 18:22:51 +0200

    Fix: Jijijiagrrrr

--- Scripts/GameManager2.0.gd
+++ Scripts/GameManager2.0.gd
@@ -332,6 +332,7 @@ func game_won():
 		$"../SoundManager/escape".play()
 		Blackluck.add_theme_color_override("font_color", "f6b400")
 		await get_tree().create_timer(10).timeout
+		$"../Start/ShakiShaki".visible = false
 		$"../End/credits".visible = true
 		var tween3 = get_tree().create_tween()
 		tween3.tween_property($"../SoundManager/credits", "volume_db", 2.811, 5.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
